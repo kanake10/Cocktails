6f55a7414b42360aaf4640c98c0949b6860eff26
kanake10 <10kanake@gmail.com>
2022-06-01 22:27:29 +0300

    commit

---
+++ cache/src/main/java/com/example/cache/dao/CockTailDao.kt
@@ -0,0 +1,18 @@
+package com.example.cache.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.cache.entity.CockTailEntity
+
+@Dao
+interface CockTailDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveCockTails(list: List<CockTailEntity>)
+
+    @Query("SELECT * FROM cocktails")
+    suspend fun getCockTails(): List<CockTailEntity>
+
+
+}
---
+++ presentation/src/main/java/com/example/presentation/adapter/CockTailAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.domain.models.Drink
+import com.example.presentation.databinding.ItemCocktailBinding
+
+class CockTailAdapter : RecyclerView.Adapter<CockTailAdapter.ImageViewHolder>() {
+    class ImageViewHolder(val binding: ItemCocktailBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallBack = object : DiffUtil.ItemCallback<Drink>() {
+        override fun areItemsTheSame(oldItem: Drink, newItem: Drink): Boolean {
+            return oldItem.idDrink == newItem.idDrink
+        }
+
+        override fun areContentsTheSame(oldItem: Drink, newItem: Drink): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallBack)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemCocktailBinding.inflate(layoutInflater, parent, false)
+        return ImageViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int): Unit =
+        with(holder.binding) {
+            val image = differ.currentList[position]
+            val context = holder.itemView.context
+            cockTailName.text = image.strDrink
+            Glide.with(context)
+                .load(image.strDrinkThumb)
+                .transform(CenterInside(), RoundedCorners(24))
+                .into(cockTailImage)
+        }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+
+}
