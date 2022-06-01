143702acf765ac617373ceeec80f3cb5ef5e0806
kanake10 <10kanake@gmail.com>
2022-06-01 22:24:59 +0300

    utils

---
+++ core/src/main/java/com/example/core/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.core
+
+object Constants {
+    const val BASE_URL = "https://thecocktaildb.com/api/json/v1/1/"
+    const val ENDPOINT = "filter.php"
+    const val COCKTAILS_DB = "cocktail.db"
+}
