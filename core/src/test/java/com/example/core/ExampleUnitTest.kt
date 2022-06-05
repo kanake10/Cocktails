d8a1ff53916d89339eeb7f3949c1dbf9184dbb14
kanake10 <10kanake@gmail.com>
2022-06-05 22:27:03 +0300

    commit

--- core/src/test/java/com/example/core/ExampleUnitTest.kt
+++ core/src/test/java/com/example/core/ExampleUnitTest.kt
@@ -1,8 +1,6 @@
 package com.example.core
-
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
