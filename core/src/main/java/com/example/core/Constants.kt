f14098c831f9dcbbe5214c49a27b7a3c4a89275c
kanake10 <10kanake@gmail.com>
2022-06-05 18:21:58 +0300

    commit

--- core/src/main/java/com/example/core/Constants.kt
+++ core/src/main/java/com/example/core/Constants.kt
@@ -1,7 +1,12 @@
 package com.example.core
 
+/**
+ * commons
+ */
 object Constants {
     const val BASE_URL = "https://thecocktaildb.com/api/json/v1/1/"
     const val ENDPOINT = "filter.php"
     const val COCKTAILS_DB = "cocktail.db"
+    const val DELAY = "3000"
+    const val DEFAULT_CALL = "Cocktail"
 }
